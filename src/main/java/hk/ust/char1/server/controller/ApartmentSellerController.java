62a71ede3c8e1cac02c03c3599e3de2d5164495e
khwongbf <khwongbf@github.com>
2019-05-13 20:10:06 +0800

    added doc

--- src/main/java/hk/ust/char1/server/controller/ApartmentSellerController.java
+++ src/main/java/hk/ust/char1/server/controller/ApartmentSellerController.java
@@ -37,6 +37,12 @@ public class ApartmentSellerController {
 		this.jwtDecoder = jwtDecoder;
 	}
 
+	/**
+	 * Puts an apartment on sale.
+	 * @param listingDetailsDTO The object that contains details for selling the apartment.
+	 * @param webRequest The web request that contains "Authorization" header with a JWT token as the value for obtaining the username and roles.
+	 * @return HTTP response that indicates whether the listing process is successful.
+	 */
 	@PostMapping("/list")
 	public ResponseEntity listApartment(@Valid @RequestBody ListingDetailsDTO listingDetailsDTO, WebRequest webRequest){
 		DecodedJWT decodedJWT = jwtDecoder.decodeFromRequest(webRequest);
@@ -56,6 +62,11 @@ public class ApartmentSellerController {
 		}
 	}
 
+	/**
+	 * Finds the apartments that the given owner is selling at the market.
+	 * @param webRequest The web request that contains "Authorization" header with a JWT token as the value for obtaining the username and roles.
+	 * @return HTTP response that contains the status code which indicates whether the query is successful, and the query results in the body if the query is successful.
+	 */
 	@GetMapping("/findSelf")
 	public ResponseEntity<List<ListingDetailsDTO>> getSelfListings(WebRequest webRequest){
 		DecodedJWT decodedJWT = jwtDecoder.decodeFromRequest(webRequest);
@@ -75,6 +86,12 @@ public class ApartmentSellerController {
 		}
 	}
 
+	/**
+	 * Modifies a listing of apartment, where the apartment is owned by a seller.
+	 * @param listingDetailsDTO The details where the listing should be changed to. The unique tag is used to search for the current apartment.
+	 * @param webRequest The web request that contains "Authorization" header with a JWT token as the value for obtaining the username and roles.
+	 * @return HTTP response that indicates whether the modification is successful.
+	 */
 	@PutMapping("/modifyListing")
 	public ResponseEntity modifyListing(@Valid @RequestBody ListingDetailsDTO listingDetailsDTO, WebRequest webRequest){
 		DecodedJWT decodedJWT = jwtDecoder.decodeFromRequest(webRequest);
@@ -94,6 +111,12 @@ public class ApartmentSellerController {
 		}
 	}
 
+	/**
+	 * Removes a sale from the current market. This action must be done by the seller who owns the apartment.
+	 * @param listingDetailsDTO The object that contains the listing details of the apartment. Only the unique tag is used in this method.
+	 * @param webRequest The web request that contains "Authorization" header with a JWT token as the value for obtaining the username and roles.
+	 * @return HTTP status that indicates whether the removal is successful.
+	 */
 	@DeleteMapping("/removeSale")
 	public ResponseEntity removeListing(@Valid @RequestBody ListingDetailsDTO listingDetailsDTO, WebRequest webRequest){
 		DecodedJWT decodedJWT = jwtDecoder.decodeFromRequest(webRequest);
